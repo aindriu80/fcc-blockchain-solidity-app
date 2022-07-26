c0527698acec0406ac2572f25527f673f65c9807
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-07-26 14:37:36 +0100

    Simple Types

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.8; // 0.8.8
 
 contract SimpleStorage{
-	
+	// Simple types
+	// boolean, uint, int, address, bytes 
+	bool hasFavouriteNumber = true;
+	uint256  favouriteNumber = 5;
+	string favouriteNumberInText = "Five";
+	int256 favouriteInt = -5;
+	address myAddress = 0x233223;
+	bytes32 favouriteBytes = "cat";
+	// This get initialed to zero
+	uint256 favouriteNumberTwo;  
+
 }
