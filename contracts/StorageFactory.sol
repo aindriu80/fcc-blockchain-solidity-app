05b281d547b4c282645f014ff43c8a5af68fd796
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-07-31 16:23:59 +0100

    Adding functions to the simple storage

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -4,9 +4,28 @@ pragma solidity ^0.8.0;
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
-	SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorage;
 
-	function createSimpleStorageContract() public {
-		simpleStorage = new SimpleStorage();
-	}
+    //  Creating simple storage contracts - then saves to array
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    // Call functions on array 
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
+        // Address
+        // ABI - Application Binary Interface
+
+        // SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        // simpleStorage.store(_simpleStorageNumber);
+	simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    // Read values from simple storage contract
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
 }
