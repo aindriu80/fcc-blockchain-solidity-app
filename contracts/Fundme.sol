d6191e8bfaa5cbfa37b2fe3abf654f423b7a94c2
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-08-01 16:25:58 +0100

    Oracles and Chainlinks

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -6,13 +6,14 @@
 pragma solidity ^0.8.0;
 
 contract FundMe {
+	uint256 public minimumUsd = 50;
    
 
     function fund() public payable {
         // Want to be able to set a minimum amount in USD
         // 1. How do we send ETH to this contract
       
-        require(msg.value > 1e18, "Didn't send enough"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000 (18 zeros)
+        require(msg.value >= minimumUsd, "Didn't send enough"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000 (18 zeros)
 	 
         //  What is reverting? -> it undos any action before, and sends remaining gas back
     }
