05e408d58e8978cb1330c831b057b5e752fbfc28
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-08-07 15:58:53 +0100

    Example of the three methods of sending Ether

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -17,9 +17,39 @@ contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!");
+        require(
+            msg.value.getConversionRate() >= minimumUsd,
+            "Didn't send enough!"
+        );
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     }
-    // function withdraw(){}
+
+    function withdraw() public {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //  reset the array
+        funders = new address[](0);
+
+        //  need to actually withdraw funds - 3 different ways: transfer, send, call
+        // Transfer - the 3 methods
+
+        // payable(msg.sender) = payable adress
+        payable(msg.sender).transfer(address(this).balance);
+
+        // Send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send Failed");
+
+        // Call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
+
+    }
 }
