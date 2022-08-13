e488f0d306251a0039e41002944d8c73f524e862
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-08-13 13:21:43 +0100

    Ganache

--- deploy.js
+++ deploy.js
@@ -1,9 +1,12 @@
 async function main() {
-
+  // compile them in our code
+  // compile them seperately
+  // http://127.0.0.1:7545
 }
+
 main()
-.then(()=>process.exit(0));
-.catch((error)=>{
-	console.log(error)
-	process.exit(1)
-});
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
